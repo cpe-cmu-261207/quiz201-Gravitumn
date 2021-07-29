6432875b96b650a284e0d3261a6668d437f06d50
leekyungwoo <thanisorn_c@cmu.ac.th>
2021-07-29 13:59:22 +0700

    test

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
-
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+
 }
 
 // more codes for Search and Reset buttons here
